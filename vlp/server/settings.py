2c999494ccc0c2fdd60881ff7f79ef6859680889
Daniel Busch <dakopen185@gmail.com>
2024-05-16 13:04:04 +0200

    changed debug

--- vlp/server/settings.py
+++ vlp/server/settings.py
@@ -35,9 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
