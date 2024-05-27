185837b7c8102dc5b77d993567dba08083527528
Vincent <Lopezray2003@gmail.com>
2024-05-28 00:11:09 +0200

    Implementing download_video (doenst work yet)

--- vlp/api/helpers.py
+++ vlp/api/helpers.py
@@ -1,3 +1,22 @@
+import yt_dlp as youtube_dl
 # Helper functions
 
-# Video Download
+download_directory = 'vlp/youtube-downloads'
+
+# Video Download function
+def download_video(url):
+    ydl_opts = {
+        'format': 'best[height<=720]',  # best quality up to 720p
+        'outtmpl': f'{download_directory}/%(id)s.%(ext)s',  # Save file as the video ID
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',  # Ensure the video is in mp4 format
+        }]
+    }
+
+    # Create a YoutubeDL object with the options
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+#Test
+# download_video("https://www.youtube.com/shorts/4YCfglGQHfI")
