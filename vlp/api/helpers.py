82e19451351cc78c0fa8140d9cbc2c04ef6e4631
Vincent <Lopezray2003@gmail.com>
2024-05-28 12:21:11 +0200

    Cleaning the Code

--- vlp/api/helpers.py
+++ vlp/api/helpers.py
@@ -3,8 +3,12 @@ import yt_dlp as youtube_dl
 
 download_directory = 'vlp/youtube-downloads'
 
-# Video Download function
+
 def download_video(url):
+    """
+    This function creates a variable ydl_opts containing how the video should be downloaded 
+    and then uses the yt_dlp module to download the video into the download_directory
+    """
     ydl_opts = {
         'format': 'best[height<=720]',  # best quality up to 720p
         'outtmpl': f'{download_directory}/%(id)s.%(ext)s',  # Save file as the video ID
