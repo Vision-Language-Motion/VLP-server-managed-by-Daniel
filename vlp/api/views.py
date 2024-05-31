5190fb6b0c7b2f201439d99ee6b705b8abf022ca
Daniel Busch <70059634+dakopen@users.noreply.github.com>
2024-05-31 15:03:28 +0200

    Update views.py so it hopefully can accept POST

--- vlp/api/views.py
+++ vlp/api/views.py
@@ -1,7 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Video
 from .serializers import VideoSerializer
 
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
