78e6334ae8380d9fbda3afa7d9be5f7c57656743
KarateKugler <milian.kugler@gmail.com>
2024-06-05 14:19:21 +0200

    Added error handling for download and delete

--- vlp/api/helpers.py
+++ vlp/api/helpers.py
@@ -1,6 +1,7 @@
 import os
 import yt_dlp as youtube_dl
 from server.settings import BASE_DIR
+import logging
 
 # Helper functions
 
@@ -10,8 +11,13 @@ download_directory = os.path.join(BASE_DIR,'youtube-downloads')
 # Download
 def download_video(url):
     """
-    This function creates a variable ydl_opts containing how the video should be downloaded 
-    and then uses the yt_dlp module to download the video into the download_directory
+    Downloads a video from the given URL using yt_dlp and saves it to the specified directory.
+
+    Args:
+        url (str): The URL of the video to download.
+
+    Returns:
+        str: The file path of the downloaded video, or None if the download failed.
     """
     ydl_opts = {
         'format': 'best[height<=720]',  # best quality up to 720p
@@ -22,22 +28,38 @@ def download_video(url):
         }]
     }
 
-    # Create a YoutubeDL object with the options
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    try:
+        # Create a YoutubeDL object with the options and download the video
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        
+        # Extract video ID and construct file path
+        video_info = youtube_dl.YoutubeDL().extract_info(url, download=False)
+        video_id = video_info['id']
+        file_path = f"{download_directory}/{video_id}.mp4"
+        
+        logging.info(f'Video downloaded successfully: {file_path}')
+        return file_path
+    except youtube_dl.utils.DownloadError as e:
+        logging.error(f'Error downloading video: {e}')
+    except Exception as e:
+        logging.error(f'An unexpected error occurred: {e}')
     
-    video_id = youtube_dl.YoutubeDL().extract_info(url, download=False)['id']
-    file_path = f"{download_directory}/{video_id}.mp4"
-    return file_path
+
+    return None
 
 
 # Delete 
 def delete(file_path):
     ''' This function checks if a file (/the directory) exists and deletes it'''
-    if (os.path.exists(file_path)):
-        os.remove(file_path)
-        return
+    try:
+        if (os.path.exists(file_path)):
+            os.remove(file_path)
+            logging.info(f'File deleted successfully: {file_path}')
+            return
+    except Exception as e:
+        logging.error(f'Error occurred while deleting file {file_path}: {e}')
  
-    print (f'File: {file_path}, does not exist')
+    logging.error(f'File: {file_path}, does not exist')
     return
     
