c94dfb668c3a21ec576275a51fa6a6d7a1f38eb3
Daniel Busch <dakopen185@gmail.com>
2024-06-05 17:32:34 +0200

    Add documentation and some slight changes

--- vlp/poseestimator/helpers.py
+++ vlp/poseestimator/helpers.py
@@ -2,11 +2,20 @@ import logging
 logger = logging.getLogger(__name__)
 
 def get_pose_inference(filepath="poseestimator/800Kräfte_auf_skifahrer_am_hang.jpeg"):
+    """Uses MMPose to infer poses from an image. Result format
+    
+    dictionary with keys:
+    visualization': None, 
+    'predictions': [[{'keypoints': [[535.6224365234375, 204.6596221923828], ...], 
+    'keypoint_scores': [0.9561091065406799, 1.0398149490356445, ...],
+    'bbox': ([97.65226745605469, 94.52516174316406, 584.6615600585938, 486.0082702636719],),
+    'bbox_score': 0.9103934
+    """
     from .apps import pose_inferencer
 
     result_generator = pose_inferencer(filepath)
     result = next(result_generator)
 
     logger.warn(result)
-
+    return result
     
