f7a713c4c88d7cb77c30d00769ce25d092278a98
Daniel Busch <dakopen185@gmail.com>
2024-06-10 21:21:29 +0200

    small changes

--- vlp/poseestimator/apps.py
+++ vlp/poseestimator/apps.py
@@ -26,5 +26,4 @@ class PoseestimatorConfig(AppConfig):
         pose2d_weights_path = f"{mmpose_model_dir}/rtmpose-l_simcc-body7_pt-body7_420e-384x288-3f5a1437_20230504.pth"
         
         pose_inferencer = MMPoseInferencer(pose2d='rtmpose-l', pose2d_weights=pose2d_weights_path, det_model = os.path.join(
-            mmpose_path, '.mim', 'demo/mmdetection_cfg/'
-            'rtmdet_m_640-8xb32_coco-person.py'), det_weights=det_weights_path, det_cat_ids=(0, ))
+            mmpose_path, '.mim', 'demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py'), det_weights=det_weights_path, det_cat_ids=(0, ))
