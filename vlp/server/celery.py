b55ae34abb6b2e9fd3f8db2730da94780dc0179e
KarateKugler <milian.kugler@gmail.com>
2024-06-19 04:24:45 +0200

    48 hour scheduled keyword search

--- vlp/server/celery.py
+++ vlp/server/celery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
@@ -6,3 +7,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 app = Celery('server')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'requeue-keywords-every-48-hours': {
+        'task': 'server.tasks.cycle_keywords',
+        'schedule': crontab(hour='*/48'),  # Run every 48 hours
+    },
+}
