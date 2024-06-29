414e47a6ebaae86152664d7e53e191e573cc2717
Vincent <Lopezray2003@gmail.com>
2024-06-29 12:40:23 +0200

    Resolving Conversation about celery.py

--- vlp/server/celery.py
+++ vlp/server/celery.py
@@ -11,13 +11,18 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'query-search-every-24-hours': {
         'task': 'api.tasks.query_search',
-        # 'schedule': crontab(hour='*/24'),  # Runs every 24 hours
-        'schedule': crontab(minute='*/1'),  # Debugging: Runs every Mintue
-    },
-    'process-urls-every-hour': {
-        'task': 'api.tasks.process_urls',
-        # 'schedule': crontab(minute=0, hour='*/1'),  # Executes every hour
-        'schedule': crontab(minute='*/1'),  # Debugging: Runs every Mintue
-
+        'schedule': crontab(hour='*/24'),  # Runs every 24 hours
+        # 'schedule': crontab(minute='*/1'),  # Debugging: Runs every Mintue
     },
 }
+
+
+# Wird auf dem Cluster processed
+'''
+    'process-urls-every-hour': {
+        'task': 'api.tasks.process_urls',
+        'schedule': crontab(minute=0, hour='*/1'),  # Executes every hour
+        # 'schedule': crontab(minute='*/1'),  # Debugging: Runs every Mintue
+
+    },
+'''
